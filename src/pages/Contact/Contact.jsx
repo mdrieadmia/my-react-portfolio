df37d5261f100e851f9c28f2432619b8a7f4f2d2
RIEAD <mdriead.bd@gmail.com>
2024-07-04 19:22:42 +0600

    Email send error issue resolved

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -3,6 +3,7 @@ import { FaEnvelope } from "react-icons/fa";
 import { IoLogoWhatsapp } from "react-icons/io";
 import { IoCall } from "react-icons/io5";
 import { FaLocationDot } from "react-icons/fa6";
+import emailjs from "@emailjs/browser"
 import { Button } from "@material-tailwind/react";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
@@ -12,9 +13,39 @@ AOS.init();
 
 const page = () => {
 
-    const handleSubmit = (e) =>{
+    const handleSubmit = (e) => {
         e.preventDefault();
-        toast.success('Message sent successfully')
+
+        const firstName = e.target.firstName.value;
+        const lastName = e.target.lastName.value;
+        const fullName = firstName + lastName;
+        const email = e.target.email.value;
+        const message = e.target.message.value;
+
+
+        const serviceId = "service_g24rgyq";
+        const templateId = "template_gufma09";
+        const publicKey = "4kmuv4jeUqx4FkAuo";
+
+        const templateParams = {
+            from_name: fullName,
+            from_email: email,
+            to_name: 'Riead',
+            message: message
+        }
+
+        emailjs.send(serviceId, templateId, templateParams, publicKey).then(
+            (respons) => {
+                console.log(respons);
+                toast.success('Message sent successfully')
+            },
+            (error) => {
+                console.log(error);
+                toast.error('Message sent failed')
+            },
+        );
+
+
     }
 
     return (
@@ -43,12 +74,12 @@ const page = () => {
                 </div>
                 <div className="w-full">
                     <form onSubmit={handleSubmit} className="grid grid-cols-2 gap-5">
-                        <input data-aos-duration="1000" data-aos="fade-up" className="block px-3 py-2 w-full rounded-lg bg-gray-800" required type="text" placeholder="First Name" />
-                        <input data-aos-duration="1000" data-aos="fade-up" className="block px-3 py-2 w-full rounded-lg bg-gray-800" required type="text" placeholder="Last Name" />
-                        <input data-aos-duration="1000" data-aos="fade-up" className="block px-3 py-2 w-full rounded-lg bg-gray-800" required type="text" placeholder="Phone Number" />
-                        <input data-aos-duration="1000" data-aos="fade-up" className="block px-3 py-2 w-full rounded-lg bg-gray-800" required type="text" placeholder="Subject" />
-                        <input data-aos-duration="1000" data-aos="fade-up" className="block px-3 py-2 w-full rounded-lg bg-gray-800 col-span-2" required type="email" placeholder="Email Address" />
-                        <textarea data-aos-duration="1000" data-aos="fade-up" className="block px-3 py-2 w-full rounded-lg col-span-2 bg-gray-800" required placeholder="Your Message" />
+                        <input data-aos-duration="1000" data-aos="fade-up" className="block px-3 py-2 w-full rounded-lg bg-gray-800" required type="text" name='firstName' placeholder="First Name" />
+                        <input data-aos-duration="1000" data-aos="fade-up" className="block px-3 py-2 w-full rounded-lg bg-gray-800" required type="text" name='lastName' placeholder="Last Name" />
+                        <input data-aos-duration="1000" data-aos="fade-up" className="block px-3 py-2 w-full rounded-lg bg-gray-800" required type="text" name='phone' placeholder="Phone Number" />
+                        <input data-aos-duration="1000" data-aos="fade-up" className="block px-3 py-2 w-full rounded-lg bg-gray-800" required type="text" name='subject' placeholder="Subject" />
+                        <input data-aos-duration="1000" data-aos="fade-up" className="block px-3 py-2 w-full rounded-lg bg-gray-800 col-span-2" required type="email" name='email' placeholder="Email Address" />
+                        <textarea data-aos-duration="1000" data-aos="fade-up" className="block px-3 py-2 w-full rounded-lg col-span-2 bg-gray-800" required name='message' placeholder="Your Message" />
                         <div data-aos-duration="1000" data-aos="fade-up" className="flex col-span-2 justify-center item-center ">
                             <Button type="submit" variant="gradient" className="normal-case">Send</Button>
                         </div>
